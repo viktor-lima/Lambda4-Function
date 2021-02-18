1635e512354ed4158896bfabecda9549a391a982
viktor-lima <joaolimak10@outlook.com>
2021-02-18 20:18:53 -0300

    Declared lambda expression

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,26 +7,24 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import utils.ToUpperCase;
 
 public class Main {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
+
 		Function<Product, String> func = p -> p.getName().toUpperCase();
-		
-		
+
 		List<String> names = list.stream().map(func).collect(Collectors.toList());
-		
+
 		names.forEach(System.out::println);
-		
+
 	}
 }
